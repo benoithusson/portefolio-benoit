109fa97fd3c1ec27a69bda2d6fa4b121f06604b3
benoithusson <benoit.husson@liip.ch>
2022-08-23 17:24:54 +0200

    Add animation for Header on load

--- components/app/Header.js
+++ components/app/Header.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
 import styles from '../../styles/components/Header.module.scss'
+import gsap from 'gsap'
 
 const Header = () => {
+
+  const ref_header = useRef(null)
+
+  useEffect(() => {
+    const el_header = ref_header.current
+
+    gsap.fromTo(el_header,
+      { opacity: 0 },
+      { opacity: 1, duration: 2, ease: 'Power2.easeIn' }
+    )
+
+  }, [])
+
+
   return (
     <>
-      <div className={styles.header__container}>
+      <div className={styles.header__container} ref={ref_header}>
         <div className={styles.header}>
           <div className={styles.header__brand}>
             <Link href="/">
