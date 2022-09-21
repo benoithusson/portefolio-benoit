a9990bd0d2c2d8bb728dcfd8769a0cbe07975397
benoithusson <benoit.husson@liip.ch>
2022-09-21 12:28:23 +0200

    Refactoring

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,10 @@ import styles from '../styles/pages/Home.module.scss'
 import skills from '../data/skills'
 import Card from '../components/ui/Card'
 import Image from 'next/image';
+import { v4 as uuidv4 } from 'uuid';
 
 gsap.registerPlugin(ScrollTrigger)
 
-// const ref_itemList = useRef();
-//     const ref_item = useRef();
-
-
-//     useEffect(() => {
-//         const el_item = ref_itemList.current;
-
-//         const tl = gsap.timeline({
-//             scrollTrigger: {
-//                 trigger: el_item,
-//                 markers: true,
-//                 start: 'top 90%',
-//                 end: 'top 20%'
-//             }
-//         });
-
-//         tl.to()
 
 export default function Home(props) {
 
@@ -38,17 +22,23 @@ export default function Home(props) {
   const ref_scroll = useRef()
 
   // Ref skills
-  const ref_skills_container = useRef()
-  const ref_skills = useRef();
-  const ref_skill = useRef();
+  const ref_skills_container = useRef();
+  const skillRefs = useRef([]);
+  skillRefs.current = [];
+
+  const addToSkillRefs = (el) => {
+    // https://www.youtube.com/watch?v=ygPIjzhKB2s
+    if (el && !skillRefs.current.includes(el)) {
+      skillRefs.current.push(el);
+    }
+    console.log(skillRefs);
+  }
 
   useEffect(() => {
     const el_left_home_card = ref_left_home_card.current
     const el_right_home_card = ref_right_home_card.current
     const el_left_home_card_content = ref_left_home_card_content.current
     const el_right_home_card_content = ref_right_home_card_content.current
-    const el_skills = ref_skills.current
-    const el_ref_skill = ref_skill.current
 
     // Animation bloc left
     gsap.to(
@@ -89,7 +79,21 @@ export default function Home(props) {
       { opacity: 0 },
       { opacity: 1, duration: 2.5, ease: 'Power2.easeIn' },
     )
+  }, [])
 
+  useEffect(() => {
+    skillRefs.current.forEach(skill => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: skill,
+          start: 'center 65%',
+          end: 'bottom 70%',
+          scrub: 1
+        }
+      })
+      tl.to(skill, { opacity: 0, duration: 1 })
+      tl.to(skill, { opacity: 1, duration: 1 })
+    })
   }, [])
 
   return (
@@ -98,29 +102,29 @@ export default function Home(props) {
 
         {/* HERO CARDS */}
         <div className={styles.leftPresentationBloc} ref={ref_left_home_card}>
-          <Card
+          {/* <Card
             classCard={ref_left_home_card}
             classText={styles.leftContent}
             refText={ref_left_home_card_content}
             text={['Frontend &', <br />, 'UX']}
             key={'test'}
-          />
+          /> */}
         </div>
         <div className={styles.rightPresentationBloc} ref={ref_right_home_card}>
-          <Card
+          {/* <Card
             classCard={ref_right_home_card}
             classText={styles.rightContent}
             refText={ref_right_home_card_content}
             text={`My name is Benoît Thiennard. I am Frontend Developer with UX Skills.
             I speak Français, English und Deutsch. I am a fan of Xtrem Sports.`}
             key={'test2'}
-          />
+          /> */}
         </div>
       </div>
 
       {/* SCROLL */}
       <div className={styles.scrollContainer} ref={ref_scroll}>
-        <Card
+        {/* <Card
           pathImage="/arrow-down.svg"
           widthImage={15}
           heightImage={15}
@@ -128,28 +132,31 @@ export default function Home(props) {
           classText={styles.scrollTextContainer}
           text="Scroll"
           key={'test3'}
-        />
+        /> */}
       </div>
 
       {/* SKILLS */}
-      <div className={styles.skillsList} ref={ref_skills_container}>
-        <h3>The Skills</h3>
+      <div className={styles.listOfSkills} ref={ref_skills_container}>
+        <h3 className={styles.titleContainerListOkSkills}>The Skills</h3>
         {
-          skills.map((skill, id) => (
-            <div className={styles.skill} key={id}>
-              <div className={styles.skillImage}>
-                <Image
-                  src={skill.path_logo}
-                  width={70}
-                  height={70}
-                  alt={skill.alt}
-                />
+          skills.map(skill => {
+            return (
+              <div className={styles.skill} key={skill.name} ref={addToSkillRefs}>
+                <div className={styles.imageSkill}>
+                  <Image
+                    src={skill.path_logo}
+                    width={90}
+                    height={70}
+                    alt={skill.alt}
+                    quality={100}
+                  />
+                </div>
+                <div className={styles.nameSkill}>
+                  <p>{skill.name}</p>
+                </div>
               </div>
-              <div className={styles.skillText}>
-                <p>{skill.name}</p>
-              </div>
-            </div>
-          ))
+            )
+          })
         }
       </div>
     </>
