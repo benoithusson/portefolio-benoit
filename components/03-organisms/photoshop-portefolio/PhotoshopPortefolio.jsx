73970d7d9ff4408bffa1c99cb6b76a156901363c
benoithusson <benoit.husson@liip.ch>
2022-10-19 12:18:39 +0200

    Finish portefolio ps on mobile

--- components/03-organisms/photoshop-portefolio/PhotoshopPortefolio.jsx
+++ components/03-organisms/photoshop-portefolio/PhotoshopPortefolio.jsx
@@ -1,67 +1,52 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import styles from './PhotoshopPortefolio.module.scss'
 import Image from 'next/image'
+import photoshopPortefolio from '../../../data/ps-portefolio'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function PhotoshopPortefolio() {
 
-    const popupRef = useRef();
+    const imageRef = useRef([]);
+    imageRef.current = [];
 
-    // Quand je clique sur une image
-    // La popup apparait
-    const handleDisplayPopup = (e) => {
-        let el_popup = popupRef.current;
-        if (e) {
-            let img = e.target;
-            let imgSrc = img.src;
-            el_popup.classList.add(styles['displayPopup']);
-            handleDisplayImage(imgSrc);
+    useEffect(() => {
+        imageRef.current.forEach(el => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: el,
+                    start: 'top 70%',
+                    end: 'center 90%',
+                }
+            })
+            tl.fromTo(el, { opacity: 0 }, { opacity: 1, duration: 0.4 })
+        })
+
+    }, []);
+
+    const addToRefs = (el) => {
+        if (el && !imageRef.current.includes(el)) {
+            imageRef.current.push(el);
         }
-    }
-
-    const handleDisplayImage = (src) => {
-        console.log(src);
-        return (
-            <>
-                <h1>YOLO</h1>
-            </>
-        )
-    }
-
-    const handleClosePopup = () => {
-        let el_popup = popupRef.current;
-        el_popup.classList.remove(styles['displayPopup'])
     }
 
     return (
         <div className={styles.containerPhotoshopPortefolio}>
-            <div className={styles.containerPhotoshopImage}>
-                <Image src="/images-ps-portefolio/charlie-bonaparte.jpg" width={1004} height={1200} onMouseEnter={(e) => handleDisplayPopup(e)} />
-                <figcaption>Figcaption</figcaption>
-            </div>
-            <div className={styles.containerPhotoshopImage}>
-                <Image src="/images-ps-portefolio/daredevil.jpg" width={1028} height={720} />
-                <figcaption>Figcaption</figcaption>
-            </div>
-            <div className={styles.containerPhotoshopImage}>
-                <Image src="/images-ps-portefolio/jeremy-joker.jpg" width={904} height={1299} />
-                <figcaption>Figcaption</figcaption>
-            </div>
-            <div className={styles.containerPhotoshopImage}>
-                <Image src="/images-ps-portefolio/wolf-patronus.jpg" width={1000} height={667} />
-                <figcaption>Figcaption</figcaption>
-            </div>
-            <div className={styles.containerPhotoshopImage}>
-                <Image src="/images-ps-portefolio/wolverine-renaud.jpg" width={700} height={1244} />
-                <figcaption>Figcaption</figcaption>
-            </div>
-            <div className={styles.popupImage} ref={popupRef}>
-                <div className={styles.containerCloseBtn} onClick={handleClosePopup}>
-                    <button className={styles.closeBtn}>X</button>
-                    <div className={styles.containerPhotoshopImage}>
-                        {handleDisplayImage}
+            {photoshopPortefolio.map(el => {
+                return (
+                    <div className={styles.containerPhotoshopImage} key={el.imagePath} ref={addToRefs}>
+                        <Image
+                            src={`/images-ps-portefolio${el.imagePath}`}
+                            width={el.width} height={el.height}
+                        />
+                        <div className={styles.containerFigcaption}>
+                            <figcaption>{el.figcaption}</figcaption>
+                        </div>
                     </div>
-                </div>
-            </div>
+                )
+            })}
         </div>
     )
 }
--- data/ps-portefolio.js
+++ data/ps-portefolio.js
@@ -1,31 +1,31 @@
 const photoshopPortefolio = [
     {
         'imagePath': '/charlie-bonaparte.jpg',
-        'figpcation': 'Charlie Bonaparte',
+        'figcaption': 'Charlie Bonaparte',
         'width': 1004,
         'height': 1200,
     },
     {
-        'imagePath': '/daredevil.jpg',
-        'figpcation': 'Resume Made in Daredevil',
-        'width': 1028,
-        'height': 720,
-    },
-    {
         'imagePath': '/jeremy-joker.jpg',
-        'figpcation': 'Joker, GOT and Rainbow',
+        'figcaption': 'Joker, GOT and Rainbow',
         'width': 904,
         'height': 1299,
     },
     {
+        'imagePath': '/daredevil.jpg',
+        'figcaption': 'Resume Made in Daredevil',
+        'width': 1028,
+        'height': 720,
+    },
+    {
         'imagePath': '/wolf-patronus.jpg',
-        'figpcation': 'Wolf Patronus',
+        'figcaption': 'Wolf Patronus',
         'width': 1000,
         'height': 667,
     },
     {
         'imagePath': '/wolverine-renaud.jpg',
-        'figpcation': 'Happy Birthday Made in Wolverine',
+        'figcaption': 'Happy Birthday Made in Wolverine',
         'width': 700,
         'height': 1244,
     }
