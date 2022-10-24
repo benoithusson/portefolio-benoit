b47ffd55957fcd933780e35ef19c077a316baf19
benoithusson <benoit.husson@liip.ch>
2022-10-24 09:45:08 +0200

    Change content projects

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         "name": "elleXX",
-        "description": "Id ex deserunt voluptate amet occaecat nostrud nulla. Fugiat occaecat est amet aute. Duis dolore magna aliquip do nulla dolore amet labore sunt amet exercitation est commodo.",
+        "description": "A blog built with Ghost headless CMS in order to help women to become financially independant.",
         "image_path": "/ellexx.svg",
         "project_url": "http://www.ellexx.com",
         "stacks": [
@@ -13,7 +13,7 @@ const projects = [
     },
     {
         "name": "CH++",
-        "description": "Id ex deserunt voluptate amet occaecat nostrud nulla. Fugiat occaecat est amet aute. Duis dolore magna aliquip do nulla dolore amet labore sunt amet exercitation est commodo.",
+        "description": "CH++ is a non-profit organization which strengthens the scientific and technological competencies of politics, authorities and civil society.",
         "image_path": "/chplusplus.svg",
         "project_url": "https://chplusplus.org/",
         "stacks": [
@@ -24,23 +24,25 @@ const projects = [
     },
     {
         "name": "Audemars Piguet",
-        "description": "Id ex deserunt voluptate amet occaecat nostrud nulla. Fugiat occaecat est amet aute. Duis dolore magna aliquip do nulla dolore amet labore sunt amet exercitation est commodo.",
+        "description": "Development of a booking system for Audemars Piguet's special events.",
         "image_path": "/audemars-piguet.svg",
         "project_url": "",
         "stacks": [
             { name: 'React' },
             { name: 'Module SCSS' },
             { name: 'Redux' },
+            { name: 'Typescript' },
         ]
     },
     {
         "name": "Frontify",
-        "description": "Id ex deserunt voluptate amet occaecat nostrud nulla. Fugiat occaecat est amet aute. Duis dolore magna aliquip do nulla dolore amet labore sunt amet exercitation est commodo.",
+        "description": "Build and implementation of React components for Frontify : A brand management plateform.",
         "image_path": "/frontify.svg",
         "project_url": "",
         "stacks": [
             { name: 'React' },
             { name: 'Module CSS' },
+            { name: 'Typescript' },
         ]
     },
 ]
